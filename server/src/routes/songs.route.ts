ae76d7870a2365fbc2356fbfc5d86fb0e6b56662
MKD-hub <mkdiab58@gmail.com>
2024-09-07 12:36:17 +0300

    removed filtering by artist

--- server/src/routes/songs.route.ts
+++ server/src/routes/songs.route.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-import Songs from '../models/song.model';
 import {
     getSongs,
     getGenres,
-    getArtists,
     getSongsByGenre,
-    getSongsByArtist,
     getAlbumByArtist,
     createSong,
     updateSong,
@@ -17,8 +14,6 @@ const router = express.Router();
 
 router.get('/songs', getSongs);
 router.get('/get_genres', getGenres);
-router.get('/get_artists', getArtists);
-router.get('/song/:artist', getSongsByArtist);
 router.get('/album/:artist', getAlbumByArtist);
 router.get('/genre/:genre', getSongsByGenre);
 router.post('/create', createSong);
